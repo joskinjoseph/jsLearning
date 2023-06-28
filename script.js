7f05c44e6537c1c002c40423c089f6f34f174f52
Akinbomi Oluwadamilare <joskinjoseph1@gmail.com>
2023-06-28 08:33:30 +0100

    Numbers and BigInts with some Maths operations

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
-console.log('External script')
+//Variables
+let num1 = 4;
+let num2 = 2;
+const num3 = 4.5;
+const num4 = -4.5;
+
+//Basic Maths
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+
+//Floating point inaccuracy
+console.log(0.1 + 0.2);
+console.log(5.01 + 0.02);
+
+//Other Operations
+
+//This only shows the remainder after division
+
+console.log(13 % 5);
+console.log(4 % 2);
+console.log(5 ** 5);
+
+// //increament and decrement
+// // num1 = num1 + 1;
+// // an alternative way of writing the above is
+//  num1++;
+//  console.log(num1);
+// // // num2 = num2 - 1;
+// // // alternative way to write this is
+//  num2--;
+//  console.log(num2);
+// // // num1 = num1 + 2;
+// // //alternative
+// // num1 += 2;
+// // console.log(num2);
+
+//comparison
+console.log(num1 > num2);
+console.log(num1 >= num2);
+console.log(num1 < num2);
+console.log(num1 <= num2);
+console.log(num1 == num2);
+console.log(num1 === num2);
+console.log(num1 != num2);
+console.log(num1 !== num2);
