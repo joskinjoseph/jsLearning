77606201dd3f7340470872a02b27ae908c8c40f3
Akinbomi Oluwadamilare <joskinjoseph1@gmail.com>
2023-06-28 09:33:27 +0100

    "IF" statements or "CONDITIONALS"

--- script.js
+++ script.js
@@ -1,57 +1,121 @@
-//function declaration
-//function helloAgain(firstName) {
- //   console.log(`Hello ${firstName}`);
-//}
-//function expression
-//const hello =function (firstName) {
-  // console.log(`Hello ${firstName}`);
-//}
-//helloAgain('Boss Man');
-//hello('Man')
+const age = 50;
 
-const hello = firstName =>  `Hello ${firstName}`;
-console.log(hello('Joskin'))
-
-//another example of the shortcut
-
-//const add = function(num1, num2) {
-    //const sum = num1 + num2;
-
-   // return sum;
-//}
+const drinkingAge = 19;
 
 
-//short cut
-const add = (num1, num2) => 
-    num1 + num2;
+// if (age >= drinkingAge) {
+
+//   console.log('I can  drink beer!!!')
+
+// } else {
+
+//   console.log('I cannot drink beer!!!')
+
+// }
+
+const passwordTypedIn = 'somepassword';
+
+const actualPassword = 'password';
 
 
+if (passwordTypedIn === actualPassword) {
 
-console.log(add(2,5));
+  console.log('Correct password, logging you in.')
 
-//IIFE -Immediately Invokable Function Expression
+} else {
 
-//(function(firstName) {
-   // console.log(`Hello ${firstName}`);
-//})();
+  console.log('Incorrect password. Please try again.')
 
-const person = {
-  firstName: 'Oluwadamilare',
-  'last-name': 'Akinbomi',
-  age: 20,
-  howOldAmI() {
-      console.log(`I am ${this.age} years old.`);
-
-  }
-};
-
-person.howOldAmI();
-
-//call back function
-const button = document.querySelector('button');
-
-const greeting = function() {
-  console.log('Hello there!');
 }
 
-button.addEventListener('click', greeting)
+
+//logical operators
+
+
+const month = 'Septemeber';
+
+const day = 31;
+
+
+if (month === 'Septemeber' && day === 31) {
+
+  console.log('It is Halloween')
+
+} else {
+
+  console.log('It is not Halloween!!!')
+
+}
+
+
+//when we have multiple instances we use LS
+
+const grade = 0;
+
+
+if (grade >= 80) {
+
+  console.log('Letter Grade: A')
+
+} else if (grade >= 70) {
+
+  console.log('Letter Grade: B')
+
+} else if (grade >= 60) {
+
+  console.log('Letter Grade: C')
+
+} else if (grade >= 50) {
+
+  console.log('Letter Grade: D')
+
+} else {
+
+  console.log('Letter Grade: F')
+
+}
+
+//Note: it start reading from up to find the most valid statement. or simply say that it start to find the first true condition from the top
+
+//Tenary ----short form of the LS
+
+//and this can only work with a single line statement
+
+//reverting back to the first code.........
+
+// if (age >= drinkingAge) {
+
+//   console.log('I can  drink beer!!!')
+
+// } else {
+
+//   console.log('I cannot drink beer!!!')
+
+// }
+
+
+age >= drinkingAge ?   console.log('I can  drink beer!!!') : console.log('I cannot drink beer!!!')
+
+// let canIDrinkMessage;
+
+// if (age >= drinkingAge) {
+
+//   canIDrinkMessage = 'I can  drink beer!!!';
+
+// } else {
+
+//   canIDrinkMessage = 'I cannot drink beer!!!';
+
+// }
+
+// let canIDrinkMessage = age >= drinkingAge
+
+//  ? 'I can  drink beer!!!': 
+
+//  'I cannot drink beer!!!';
+
+ console.log(age >= drinkingAge
+
+    ? 'I can  drink beer!!!': 
+
+    'I cannot drink beer!!!');
