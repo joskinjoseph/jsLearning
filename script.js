3e0c59b9e44e45dae423a9443d87ad177a7c889e
Akinbomi Oluwadamilare <joskinjoseph1@gmail.com>
2023-06-28 09:20:23 +0100

    Intro to Functions in JavaScript

--- script.js
+++ script.js
@@ -1,45 +1,46 @@
+// function declaration / function definition
+function sayHello(firstName = 'Joseph', lastName = 'Oluwadamilare') {
+    //functionb body
+    console.log('Hello',);
+    console.log(firstName);
+    console.log(lastName);
+}
+
+// function call / invoke / executing / run
+sayHello('Samuel', 'Akinbomi')
+sayHello('Favour', 'Mendy')
+sayHello();
+
+function add(num1, num2) {
+    const sum = num1 + num2;
+
+    // if the function wnt to return any value, the we use the return keyword at the end of the function
+
+return sum;
+}
+
+const result = add(2, 3);
+console.log(result);
+
+
+function sayHelloAgain(firstName = 'Joseph', lastName = 'Oluwadamilare') {
+    return `Hello, ${firstName} ${lastName}`;
+}
+
+
 const person = {
     firstName: 'Oluwadamilare',
     'last-name': 'Akinbomi',
     age: 20,
-    isCool: false,
-    hobbies: ['video games', 'watching movie', 'sleeping'],
     address: {
-        street: '3 Dada st.',
+        street: '12 Dada street',
         city: 'Lagos',
         country: 'Nigeria',
     },
     howOldAmI: function() {
-        console.log(`I am ${age} years old.`);
+        console.log(`I am ${this.age} years old.`);
 
     }
  };
 
- //console.log(person.firstName)
- // console.log(person['last-name']);
-
-person.hairColor ='black';
-
-delete person.isCool;
-
-//person.howOldAmI();
-
- //console.log(person)
-
- const groupOfPeople = [
-    {
-        name: 'Joskin',
-        age: 20,
-    },
-    {
-        name: 'Samuel',
-        age: 22,
-    },
-    {
-        name: 'Favour',
-        age: 30,
-    },
-
- ];
-
- console.log(groupOfPeople);
+ person.howOldAmI();
