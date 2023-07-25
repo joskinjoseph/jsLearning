64fe421d10267ce20b4c20216493713276002624
Akinbomi Oluwadamilare <joskinjoseph1@gmail.com>
2023-07-25 02:36:41 +0100

    Understanding Scopes

--- script.js
+++ script.js
@@ -1,78 +1,59 @@
-// for loop
+//Global scope
 
-// for (let i = 0; i < 10; i++) {
-//   console.log (`i is ${i}`);
+// let message = 'hello';
+// var greeting = 'hi';
+
+
+// console.log(message);
+// console.log(greeting);
+
+// if (true) {
+// console.log(message);
+// console.log(greeting);
 // }
 
-// const people = ['Andre', 'Bob', 'Sally'];
 
-// for (let i = 0; i < people.length; i++) {
-//   console.log (people[i]);
-// }
+//Function Scope
 
-//Break and Continue
+// function sayHello() {
+//   let message = 'hello';
+//   var greeting = 'hi';
 
-// for (let i = 0; i < 10; i++) {
-//   if (i === 4) {
-//     braek;
+//   console.log(message);
+//   console.log(greeting);
+
+//   function nested() {
+//     console.log(message);
+//   console.log(greeting);
 //   }
+//   nested();
+// }
 
+// sayHello();
+
+//Block Scope
+
+// if (true) {
+//   let message = 'hello';
+//   var greeting = 'hi';
+
+//   console.log(message);
+//   console.log(greeting);
+// }
+
+// let message = 'joskin';
+
+// console.log(message);
+
+
+// for (let i = 0; i < 5; i++) {
 //   console.log(`i is ${i}`);
-//}
-
-// for (let i = 0; i < 10; i++) {
-//   if (i === 4) {
-//     continue;
-//   }
-
-//   console.log(`i is ${i}`);
 // }
 
-//While Loops
+// console.log(i);
 
-// let j = 0;
-// while (j < 10) {
-//   console.log(`j is ${j}`);
-//   j = j + 2;
-// }
 
-//do while loop
-
-// let k = 10 - 5;
-
-// do {
-//   console.log(`k is ${k}`);
-//   k++;
-// } while (k < 10)
-
-//for in loops
-
-// const person = {
-//   firstName: 'Andre',
-//   lastName: 'Madarang',
-//   job: 'web Developer',
-// };
-
-// for(const x in person) {
-//   console.log(person[x]);
-// }
-
-//for of loops
-const people = ['Andre', 'Bob', 'Sally'];
-
-// for(const y of people) {
-//   console.log(y);
-// }
-
-//foreach
-
-// people.forEach((person, index) => {
-//   console.log(`Index ${index}: ${person}`);
-// });
-
-//map
-const peopleMap = people.map((person, index) => {
-  return `${index}: ${person}`;
-});
-
-console.log(peopleMap);
+(function() {
+  const alert = 'error';
+  console.log(alert);
+}) ();
